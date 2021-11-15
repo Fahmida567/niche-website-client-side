04d7af0d750f0347ccf6094b596a5b1b948d6869
Turin <you@example.com>
2021-11-16 01:07:38 +0600

    first commit

---
+++ src/pages/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5j3vye2aY6uT6dUt_uWbNgenGDcVU8PE",
+    authDomain: "motorbike-shop-website-21b0a.firebaseapp.com",
+    projectId: "motorbike-shop-website-21b0a",
+    storageBucket: "motorbike-shop-website-21b0a.appspot.com",
+    messagingSenderId: "1019779616204",
+    appId: "1:1019779616204:web:05264101ff968c1ab2e169"
+};
+export default firebaseConfig;
