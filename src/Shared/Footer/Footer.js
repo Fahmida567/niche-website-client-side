84519aa2fe66739be29e5e524fd4cc176f410273
Turin <you@example.com>
2021-11-27 13:01:40 +0600

    complete

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <Box>
-            <Typography variant='h4' sx={{ color: 'white', fontWeight: 'medium', mt: 2, backgroundColor: 'black', textAlign: 'center' }}>All Rights Reserved © 2021 Hero Moto Shop</Typography>
+            <Typography variant='h4' sx={{ color: 'white', fontWeight: 'medium', mt: 2, backgroundColor: 'black', textAlign: 'center' }}>All Rights Reserved © 2021 Moto Shop</Typography>
 
         </Box>
 
